bd80a44334d062db29fb8d271492dadfad1696a9
marentes <marentes_199@hotmail.com>
2019-11-03 21:46:53 -0600

    black_jack

---
+++ app/src/main/java/com/example/black_jack/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.black_jack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected RequestQueue fRequestQueue;
+    private VolleyS volley;
+    Integer suma=0;
+
+    String url= "http://ramiro174.com/api/numero";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        volley= VolleyS.getInstance(this.getApplicationContext());
+
+        fRequestQueue=volley.getRequestQueue();
+
+    }
+    @Override
+    public void onClick(View view) {
+        obtenerNumero();
+    }
+
+
+    private void obtenerNumero(){
+        JsonObjectRequest request =  new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                TextView text = findViewById(R.id.num);
+                try{
+                    Integer num = response.getInt("numero");
+                    suma += num;
+                    Toast.makeText(MainActivity.this, suma.toString(), Toast.LENGTH_SHORT).show();
+                    if(suma > 21){
+                        Toast.makeText(MainActivity.this, "Perdiste!", Toast.LENGTH_SHORT).show();
+                        text.setText("0");
+                        suma = 0;
+                        return;
+                    }
+
+                    text.setText(suma.toString());
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("OnErrorResponse: ", error.toString());
+            }
+        });
+        fRequestQueue.add(request);
+
+    }
+}
