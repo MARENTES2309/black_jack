cf659544090a5155608b40d9c5e3aafa006a05a5
marentes <marentes_199@hotmail.com>
2019-11-08 12:09:28 -0600

    black_jack 2.0

--- app/src/main/java/com/example/black_jack/MainActivity.java
+++ app/src/main/java/com/example/black_jack/MainActivity.java
@@ -23,34 +23,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected RequestQueue fRequestQueue;
     private VolleyS volley;
-    Integer suma=0;
 
-    String url= "http://ramiro174.com/api/numero";
+    Integer suma = 0;
+
+    String url = "http://ramiro174.com/api/numero";
+    String url2 = "http://ramiro174.com/api/enviar/numero";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        volley= VolleyS.getInstance(this.getApplicationContext());
+        volley = VolleyS.getInstance(this.getApplicationContext());
 
-        fRequestQueue=volley.getRequestQueue();
+        fRequestQueue = volley.getRequestQueue();
 
     }
+
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.uno:
         obtenerNumero();
+            break;
+            case  R.id.dos:
+        enviarnumero();
+        break;
+        }
     }
 
 
-    private void obtenerNumero(){
-        JsonObjectRequest request =  new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
+    private void obtenerNumero() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 TextView text = findViewById(R.id.num);
-                try{
+                try {
                     Integer num = response.getInt("numero");
                     suma += num;
                     Toast.makeText(MainActivity.this, suma.toString(), Toast.LENGTH_SHORT).show();
-                    if(suma > 21){
+                    if (suma > 21) {
                         Toast.makeText(MainActivity.this, "Perdiste!", Toast.LENGTH_SHORT).show();
                         text.setText("0");
                         suma = 0;
@@ -59,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     text.setText(suma.toString());
 
-                }catch (JSONException e){
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
@@ -72,4 +83,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fRequestQueue.add(request);
 
     }
+
+    public void enviarnumero() {
+         final JSONObject data = new JSONObject();
+        try {
+            data.put("nombre", "Marentes");
+            data.put("numero", suma);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url2, data, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                    Toast.makeText(MainActivity.this, "se envio datos", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("OnErrorResponse1: ", error.toString());
+            }
+        });
+        fRequestQueue.add(jsonObjectRequest);
+    }
+
 }
